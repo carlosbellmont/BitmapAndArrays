6c7825db1830682ed8dfb1da6d4d4b65a4c0efc9
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-03-08 20:48:01 +0100

    Initial commit

---
+++ app/src/main/java/com/cbellmont/cameraexample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.cbellmont.cameraexample
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import com.cbellmont.cameraexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bDownload.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            resultGallery.launch(intent)
+        }
+
+        binding.bCamera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            resultCamera.launch(intent)
+        }
+    }
+
+    private val resultGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            binding.ivPicture.setImageURI(result.data?.data)
+        }
+    }
+
+    private val resultCamera  = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val bitmap = result.data?.extras?.get("data") as Bitmap
+            binding.ivPicture.setImageBitmap(bitmap) 
+        }
+    }
+}
